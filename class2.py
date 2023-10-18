1d2605c0bd5a2a35951a4e8e069d54402f9c6688
Malik Abid <malikabidzz0306@gmail.com>
2023-10-18 23:00:14 +0500

    update

---
+++ class2.py
@@ -0,0 +1,30 @@
+# name : str = "Abid Malik"
+# fname : str = "Abdul Jabbar"
+# education : str = "BS Computer"
+# age : int =  23
+
+# card : str = f"""PIAIC Student Card
+# Student Name: {name}
+# Father Name: {fname}
+# Education: {education}
+# Age: {age}
+# Total Number: {60 + 50 + 40 + 30 + 20 + 10}
+# """
+
+
+# print(card)
+
+
+# a = 10
+# b = 20
+
+# print("value a = {} and value b = {}".format(a,b))
+
+student_code : str = """
+print('Student Name is Abid Malik')
+a:int = 10
+b:int = 20
+print(a+b)
+"""
+
+exec(student_code)
