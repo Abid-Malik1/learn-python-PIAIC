c49686764edc280f96178e7ae045a23fa243608b
Malik Abid <malikabidzz0306@gmail.com>
2023-10-19 21:59:57 +0500

    update

---
+++ class3.py
@@ -0,0 +1,27 @@
+# Python divides the operators in the following groups:
+
+# Arithmetic operators
+# Assignment operators
+# Comparison operators
+# Logical operators
+# Identity operators
+# Membership operators
+# Bitwise operators
+
+
+# import pandas as pd
+
+# table = pd.read_html("https://www.w3schools.com/python/python_operators.asp")
+# table[0]
+
+a : int = 10
+b : int = 20
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+print(a // b)
+
