c8a94bbd92fa1013501a1bbe8b5814338ad60c2e
Abid Malik <malikabidzz0306@gmail.com>
2024-04-04 11:22:39 +0500

    update

---
+++ fast-api/todos/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def helloWorld():
+    return"Hello Python Baby"
+@app.post("/")
+def helloWorld():
+    return"Post Hello Python Baby"
+
+@app.delete("/")
+def helloWorld():
+    return"Hello Python Baby is deleted"
+@app.put("/")
+def helloWorld():
+    return"Put is called baby"
+
+@app.get("/gettodos")
+def getTodos():
+    print("Get Todos is called")
+    return"todos is called"
+
+@app.post("/posttodos")
+def getTodos():
+    print("Get post Todos is called")
+    return"post todos is called"
+
+@app.get("/getsingletodos")
+def getSingleTodo():
+    print("Get  Single Todo is called")
+    return "Get Single Todo is Called"
+
+
+def start():
+    uvicorn.run("todos.main:app",host="127.0.0.1",port=7990, reload=True)
